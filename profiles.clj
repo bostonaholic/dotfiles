5e255c5121cae5894da627658c37e40cf6362630
Matthew Boston <matthew.boston@gmail.com>
2013-06-04 19:48:21 -0400

    add leiningen user profile

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[lein-kibit "0.0.7"]
+                  [lein-pprint "1.1.1"]]
+        :dependencies [[slamhound "1.3.3"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
