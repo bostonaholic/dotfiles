36c1c583a9a6f022705a9d0c1c7c7dbdf8128466
Matthew Boston <matthew.boston@gmail.com>
2013-07-25 21:16:43 -0400

    update versions on lein profiles

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-pprint "1.1.1"]]
-        :dependencies [[slamhound "1.3.3"]]
+        :dependencies [[slamhound "1.3.3"]
+                       [criterium "0.4.1"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
