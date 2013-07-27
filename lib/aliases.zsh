b0cf101719f4e0ca9a30c019c8424a2a0bd430f0
Matthew Boston <matthew.boston@gmail.com>
2013-07-27 11:17:05 -0400

    vim alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -11,6 +11,7 @@ aliaz rl "reload"
 alias sudo="sudo " # allow running sudo against an alias
 aliaz redo '`\history -n | tail -n1`' # run last command again
 aliaz now "sudo redo" # I meant sudo on that last command
+aliaz v "mvim $*"
 alias e="emacs $*"
 
 # Git
