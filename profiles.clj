74b360843add83090f45898294219b26dfb78a3d
Matthew Boston <matthew.boston@gmail.com>
2013-08-05 17:18:47 -0400

    add lein-exec to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,7 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [lein-pprint "1.1.1"]]
+                  [lein-pprint "1.1.1"]
+                  [lein-exec "0.3.0"]]
         :dependencies [[slamhound "1.3.3"]
                        [criterium "0.4.1"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
