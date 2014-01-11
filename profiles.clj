192fec878b868815ae833fc680c95eeffaa44f0d
Matthew Boston <matthew.boston@gmail.com>
2014-01-10 19:48:52 -0500

    update user leiningen profile libs

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-pprint "1.1.1"]
-                  [lein-exec "0.3.0"]]
-        :dependencies [[slamhound "1.3.3"]
-                       [criterium "0.4.1"]]
+                  [lein-exec "0.3.1"]]
+        :dependencies [[slamhound "1.5.0"]
+                       [criterium "0.4.2"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
