242aa1d356dc0062f9ef08fd82421b92f66826ad
Matthew Boston <matthew.boston@gmail.com>
2014-01-28 11:41:01 -0500

    update lein profile to use RELEASE of plulgins

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-{:user {:plugins [[lein-kibit "0.0.8"]
+{:user {:plugins [[lein-kibit "RELEASE"]
                   [lein-pprint "1.1.1"]
-                  [lein-exec "0.3.1"]]
-        :dependencies [[slamhound "1.5.0"]
-                       [criterium "0.4.2"]]
+                  [lein-exec "RELEASE"]]
+        :dependencies [[slamhound "RELEASE"]
+                       [criterium "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
