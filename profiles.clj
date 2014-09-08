afb20e865dc20260f49f21b939666939012ba035
Matthew Boston <matthew.boston@gmail.com>
2014-09-08 10:40:50 -0600

    add clojure plugins to profile

--- profiles.clj
+++ profiles.clj
@@ -2,12 +2,14 @@
 {:user {:plugins [[lein-kibit "RELEASE"]
                   [lein-pprint "1.1.1"]
                   [lein-exec "RELEASE"]
-                  [lein-immutant "RELEASE"]
-                  [lein-ns-dep-graph "0.1.0-SNAPSHOT"]]
+                  [lein-immutant "1.2.2"]
+                  [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
+                  [lein-cljsbuild "RELEASE"]
+                  [com.cemerick/clojurescript.test "0.3.1"]]
         :dependencies [[slamhound "RELEASE"]
                        [criterium "RELEASE"]
                        #_[datomic-schema-grapher.core "0.0.1" :exclusions [com.datomic/datomic-free]]
-                       #_[com.datomic/datomic-free "RELEASE"]]
+                       [com.datomic/datomic-free "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
 
 (defn wat
