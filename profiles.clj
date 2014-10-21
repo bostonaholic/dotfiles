eb2c00139697c9b124019412bbc7e10ff757dbdb
Matthew Boston <matthew.boston@gmail.com>
2014-10-21 16:42:25 -0600

    more clojure plugins

--- profiles.clj
+++ profiles.clj
@@ -1,15 +1,19 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-{:user {:plugins [[lein-kibit "RELEASE"]
+{:user {:plugins [;[cider/cider-nrepl "0.7.0"]
+                  ;[org.clojure/tools.nrepl "0.2.3"]
+                  [lein-cloverage "1.0.2"]
+                  [lein-kibit "0.0.8"]
                   [lein-pprint "1.1.1"]
-                  [lein-exec "RELEASE"]
+                  [lein-exec "0.3.4"]
                   [lein-immutant "1.2.2"]
                   [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
-                  [lein-cljsbuild "RELEASE"]
+                  [lein-cljsbuild "1.0.3"]
                   [com.cemerick/clojurescript.test "0.3.1"]]
-        :dependencies [[slamhound "RELEASE"]
-                       [criterium "RELEASE"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.3"]
+                       [slamhound "1.5.5"]
+                       [criterium "0.4.3"]
                        #_[datomic-schema-grapher.core "0.0.1" :exclusions [com.datomic/datomic-free]]
-                       [com.datomic/datomic-free "RELEASE"]]
+                       [com.datomic/datomic-free "0.9.4956" :exclusions [joda-time]]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
 
 (defn wat
