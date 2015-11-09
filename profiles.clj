a9c7ea06cba484f5a9380fbd93c9c6e0d264f913
Matthew Boston <matthew.boston@gmail.com>
2015-11-09 16:55:06 -0700

    add lein-ancient and lein-pprint to profile

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,8 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [lein-kibit "0.1.2"]]
+                  [lein-ancient "0.6.8"]
+                  [lein-kibit "0.1.2"]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
                        [org.clojure/tools.nrepl "0.2.11"]
                        [slamhound "1.5.5"]]
