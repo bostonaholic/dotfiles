dd5427e17374b8501fa383769fd48511dc9ff589
Matthew Boston <matthew.boston@gmail.com>
2015-11-17 19:40:30 -0700

    add helpful comments for starting emacsclient

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -11,8 +11,8 @@ alias rl="reload"
 alias mci="mvn clean install $*"
 alias e="emacs $*"
 alias ed="emacs --debug-init $*"
-alias et="emacsclient -t $*"
-alias ec="emacsclient -c $*"
+alias et="emacsclient --tty $*" # open a new Emacs frame on the current terminal
+alias ec="emacsclient --create-frame $*" # create a new frame instead of trying to use the current Emacs frame
 alias gpg=gpg2
 
 # Git
