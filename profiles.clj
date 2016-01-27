ac97642d98212facb9a35ac91845faaf4218e4a2
Matthew Boston <matthew.boston@gmail.com>
2016-01-27 11:57:15 -0700

    remove datomic profile

--- profiles.clj
+++ profiles.clj
@@ -2,8 +2,7 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
                   [lein-kibit "0.1.2"]
                   [lein-pprint "1.1.2"]]
-        :dependencies [[com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
-                       [slamhound "1.5.5"]]
+        :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [com.cemerick/piggieback "0.2.1"]]
