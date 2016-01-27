c20c5857803ab027ad73a54be3dc061345412cb7
Matthew Boston <matthew.boston@gmail.com>
2016-01-27 11:56:47 -0700

    add :repl leiningen profile

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,11 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-{:user {:plugins [[cider/cider-nrepl "0.10.0"]
-                  [lein-ancient "0.6.8"]
+{:user {:plugins [[lein-ancient "0.6.8"]
                   [lein-kibit "0.1.2"]
                   [lein-pprint "1.1.2"]]
         :dependencies [[com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
-                       [org.clojure/tools.nrepl "0.2.12"]
                        [slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [com.cemerick/piggieback "0.2.1"]]
+        :plugins [[cider/cider-nrepl "0.10.2"]]
+        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
