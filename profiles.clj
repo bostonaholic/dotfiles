b0adc08696235ca40a3d788904c24e992769d3e7
Matthew Boston <matthew@matthewboston.com>
2016-06-21 14:37:02 -0600

    use clojure 1.9.0-alpha7 for repl

--- profiles.clj
+++ profiles.clj
@@ -6,7 +6,8 @@
         :dependencies [[criterium "0.4.4"]
                        [slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-alpha7"]
+                       [org.clojure/tools.nrepl "0.2.12"]
                        [com.cemerick/piggieback "0.2.1"]]
         :plugins [[cider/cider-nrepl "0.12.0"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
