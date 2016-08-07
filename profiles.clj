61ed6fc6656a41a8b2772c02c1fcbecb5cdfec78
Matthew Boston <matthew@matthewboston.com>
2016-08-06 20:44:00 -0600

    update cider-nrepl 0.13.0

--- profiles.clj
+++ profiles.clj
@@ -10,5 +10,5 @@
                        [org.clojure/data.csv "0.1.3"]
                        [org.clojure/tools.nrepl "0.2.12"]
                        [com.cemerick/piggieback "0.2.1"]]
-        :plugins [[cider/cider-nrepl "0.12.0"]]
+        :plugins [[cider/cider-nrepl "0.13.0"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
