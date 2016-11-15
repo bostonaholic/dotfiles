704bc0235a99aae3b8f1dc80324ccce5c7a77000
Matthew Boston <matthew@matthewboston.com>
2016-11-14 18:06:35 -0700

    use trusty circle build image

--- lib/ci.zsh
+++ lib/ci.zsh
@@ -1,5 +1,7 @@
 function ci() {
   set_env_var CIRCLECI_HOME $HOME'/code/circleci'
+  set_env_var CIRCLE_CONTAINER_IMAGE_URI "docker://circleci/build-image:trusty-654-3645de6"
+
   cd $CIRCLECI_HOME
 
   # PWD
