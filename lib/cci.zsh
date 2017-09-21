48dd5db593c1c8d34ff724581953549419b1058d
Matthew Boston <matthew@matthewboston.com>
2017-09-21 16:54:06 -0600

    remove CircleCI and Hendrick config

--- lib/cci.zsh
+++
@@ -1,9 +0,0 @@
-function cci() {
-  set_env_var CIRCLECI_HOME $HOME'/code/circleci'
-  set_env_var CIRCLE_CONTAINER_IMAGE_URI "docker://circleci/build-image:trusty-654-3645de6"
-
-  cd $CIRCLECI_HOME
-
-  # PWD
-  show_env_var PWD
-}
