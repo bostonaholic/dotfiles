3a0b0101016b4b50c5d1c9e1feef5dcaa28406b6
Matthew Boston <matthew@matthewboston.com>
2017-10-25 23:00:38 -0600

    update deps in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,12 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
 {:user {:plugins [[lein-ancient "0.6.12"]
-                  [atroche/lein-ns-dep-graph "0.2.0-SNAPSHOT"]
-                  [lein-hiera "0.9.5"]
-                  [lein-kibit "0.1.5"]
-                  [lein-pprint "1.1.2"]]
+                  [lein-kibit "0.1.5"]]
         :dependencies [[criterium "0.4.4"]
                        [slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :injections [(defn hello [name] (println (str "Hello, " name)))]}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-beta1"]
-                       [org.clojure/data.csv "0.1.4"]
+ :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-beta3"]
                        [org.clojure/tools.nrepl "0.2.13"]
                        [org.clojure/tools.namespace "0.2.11"]
-                       [com.cemerick/piggieback "0.2.1"]
                        [criterium "0.4.4"]]
-        :plugins [[cider/cider-nrepl "0.15.0"]]}}
+        :plugins [[cider/cider-nrepl "0.15.1"]]}}
