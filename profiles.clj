7e5e36a71657521178c716889b3ecfd7518b02fa
Matthew Boston <matthew@matthewboston.com>
2018-04-24 11:35:17 -0700

    improve lein proviles

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,20 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-{:user {:plugins [[lein-ancient "0.6.12"]
-                  [lein-kibit "0.1.5"]]
-        :dependencies [[criterium "0.4.4"]
+{:user {:pedantic? :ranges
+        :plugins [[lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.5"]
+                  [atroche/lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+                  [lein-nsorg "0.1.3"]]
+        :dependencies [[jonase/eastwood "0.2.5"]
+                       [pjstadig/humane-test-output "0.8.3"]
                        [slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :injections [(defn hello [name] (println (str "Hello, " name)))]}
- :repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-beta3"]
-                       [org.clojure/tools.nrepl "0.2.13"]
-                       [org.clojure/tools.namespace "0.2.11"]
-                       [criterium "0.4.4"]]
-        :plugins [[cider/cider-nrepl "0.15.1"]]}}
+        :injections [(defn hello [name] (println (str "Hello, " name)))
+
+                     (require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+ :repl {:plugins [[cider/cider-nrepl "0.16.0"]]
+        :dependencies [#_^:displace [org.clojure/clojure "pp1.9.0"]
+                       [criterium "0.4.4"]
+                       [cheshire "5.8.0"]
+                       [org.clojure/tools.nrepl "0.2.13"]]
+        :injections [(require '[cheshire.core :as json])]}}
