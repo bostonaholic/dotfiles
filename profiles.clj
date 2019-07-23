a34a77f8e7837514517072768a0bd37c53b2e5c8
Matthew Boston <matthew@matthewboston.com>
2019-07-22 19:50:02 -0600

    remove humane-test-output from profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -5,8 +5,7 @@
                   [lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]
                   [lein-nsorg "0.2.0"]]
-        :dependencies [[pjstadig/humane-test-output "0.8.3"]
-                       [slamhound "1.5.5"]]
+        :dependencies [[slamhound "1.5.5"]]
         :injections [(defn hello [name] (println (str "Hello, " name)))
                      (defn spongebobify [s] (apply str (map #((rand-nth [clojure.string/upper-case clojure.string/lower-case]) %) s)))
                      (defn median [coll]
@@ -17,10 +16,7 @@
                            (let [a (nth sorted (dec halfway))
                                  b (nth sorted halfway)
                                  average (fn [x y] (/ (+ x y) 2))]
-                             (average a b)))))
-
-                     (require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
+                             (average a b)))))]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
  :repl {:dependencies [#_^:displace [org.clojure/clojure "1.10.0"]
                        [cheshire "5.8.0"]
