825f441ac82e52a7197c73083c08c609da6898ed
Matthew Boston <matthew@matthewboston.com>
2020-03-25 17:08:01 -0600

    slim down lein profiles

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,12 @@
 ;; ln -s $PWD/profiles.clj ~/.lein/profiles.clj
-{:user {:pedantic? :ranges
-        :plugins [[atroche/lein-ns-dep-graph "0.2.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.22.3"]
-                  [lein-ancient "0.6.15"]
-                  [lein-kibit "0.1.6"]
-                  [lein-nsorg "0.2.0"]]
-        :dependencies [[slamhound "1.5.5"]
-                       [org.clojure/tools.nrepl "0.2.13"]
+{:user {:plugins [[atroche/lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.24.0"]
+                  [lein-ancient "0.6.15"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [nrepl "0.6.0"]]
         :injections [(defn hello [name] (println (str "Hello, " name)))
-                     (defn median [coll]
                      (defn spongemock [s] (apply str (map #((rand-nth [clojure.string/upper-case clojure.string/lower-case]) %) s)))
+                     (defn median [coll] ;; FIXME: why?
                        (let [sorted (sort coll)
                              halfway (/ (count coll) 2)]
                          (if (odd? (count coll))
@@ -18,10 +14,4 @@
                            (let [a (nth sorted (dec halfway))
                                  b (nth sorted halfway)
                                  average (fn [x y] (/ (+ x y) 2))]
-                             (average a b)))))]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
- :repl {:dependencies [#_^:displace [org.clojure/clojure "1.10.0"]
-                       [cheshire "5.8.0"]
-                       [criterium "0.4.4"]
-                       [org.clojure/tools.nrepl "0.2.13"]]
-        :injections [(require '[cheshire.core :as json])]}}
+                             (average a b)))))]}}
