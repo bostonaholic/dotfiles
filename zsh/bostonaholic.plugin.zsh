e8b02db69eac280a3cd2800215c6d9eaaa13abf2
Matthew Boston <matthew@matthewboston.com>
2021-01-20 09:16:58 -0700

    remove useless helper functions

--- zsh/bostonaholic.plugin.zsh
+++ zsh/bostonaholic.plugin.zsh
@@ -34,10 +34,3 @@ alias bundle_close="bundle exec gem pristine $*"
 # Clojure
 #alias clj="lein repl $*"
 alias cljs="planck $*"
-
-function show_env_var() { echo "$1=`printenv $1`" }
-
-function set_env_var() {
-    export $1=$2
-    show_env_var $1
-}
