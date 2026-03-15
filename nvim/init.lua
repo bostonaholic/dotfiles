6adbb00c62d1b7331709114cc926b8e5220b7273
Matthew Boston <matthew@matthewboston.com>
2026-03-14 22:59:42 -0600

    feat(nvim): add Telescope fuzzy finder with fzf-native
    Adds file finding, live grep, buffer switching, and help search
    via space-prefixed keybindings. Sets mapleader to space so
    Telescope mappings work intuitively.

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,4 +34,35 @@ require("lazy").setup({
       vim.cmd.colorscheme("catppuccin")
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = {
+            prompt_position = "top",
+          },
+        },
+      })
+      telescope.load_extension("fzf")
+    end,
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+      { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Grep word under cursor" },
+    },
+  },
 })
